506a4b19fca2e6b48dbc2238a4d460e954d6f85e
Yevhen Herasimov <yevhen.gerasimov@gmail.com>
2024-06-02 17:54:57 +0300

    Download pretrained models script, refactor

---
+++ setup/download_pretrained_models.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Download pretrained detection model
+wget -P pretrained_models/ https://paddleocr.bj.bcebos.com/PP-OCRv3/english/en_PP-OCRv3_det_distill_train.tar
+# Download pretrained recognition model
+wget -P pretrained_models/ https://paddleocr.bj.bcebos.com/PP-OCRv4/english/en_PP-OCRv4_rec_train.tar
+
+# Extract the downloaded files and clean up
+cd ./pretrained_models
+tar -xf en_PP-OCRv3_det_distill_train.tar && rm -rf en_PP-OCRv3_det_distill_train.tar
+tar -xf en_PP-OCRv3_det_distill_train.tar && rm -rf en_PP-OCRv3_det_distill_train.tar
+cd ..
+
+
